0eb0e7e594abd59a58fa62c1cf7b67b903f4e352
Denis Lykov <cailean393@gmail.com>
2020-09-03 05:16:11 +0300

    wrap game by gameSnake class

---
+++ src/core/gameSnake.ts
@@ -0,0 +1,52 @@
+import { Game } from './game';
+import { KEY } from './input';
+import { SnakeDirection, GameOptions, ObjectTypes } from './../interfaces';
+import { Apple } from './../objects/apple';
+import { Snake } from './../objects/snake';
+
+export class GameSnake {
+  private game: Game<GameOptions>;
+
+  constructor(canvas: HTMLCanvasElement, options?: GameOptions) {
+    const opts = Object.assign({}, this.defaultOptions, options || {});
+    this.game = new Game<GameOptions>(canvas, opts);
+    this.addObjects();
+    this.setInput();
+  }
+
+  public start() {
+    this.game.start();
+  }
+
+  private get defaultOptions() {
+    const options: GameOptions = { size: 15, snakeLength: 5, timeThreshold: 50 };
+    return options;
+  }
+
+  private addObjects() {
+    const apple = new Apple<GameOptions>(this.game);
+    const snake = new Snake<GameOptions>(this.game);
+    this.game.addObject(apple);
+    this.game.addObject(snake);
+  }
+
+  private setInput() {
+    const snake = this.game.objects.find(obj => obj.type === ObjectTypes.SNAKE) as Snake<GameOptions>;
+    this.game.input.onkeydown(params => {
+      switch(params.key) {
+        case KEY.ARROW_LEFT:
+          snake.turn(SnakeDirection.LEFT);
+          break;
+        case KEY.ARROW_UP:
+          snake.turn(SnakeDirection.UP);
+          break;
+        case KEY.ARROW_RIGHT:
+          snake.turn(SnakeDirection.RIGHT);
+          break;
+        case KEY.ARROW_DOWN:
+          snake.turn(SnakeDirection.DOWN);
+          break;
+      }
+    });
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,5 @@
-import { Game } from './core/game';
-import { KEY } from './core/input';
-import { SnakeDirection, GameOptions } from './interfaces';
-import { Apple } from './objects/apple';
-import { Snake } from './objects/snake';
+import { GameSnake } from './core/gameSnake';
 
 const canvas = document.querySelector('#stage') as HTMLCanvasElement;
-
-const options: GameOptions = { size: 15, snakeLength: 5, timeThreshold: 50 };
-const game = new Game<GameOptions>(canvas, options);
-const apple = new Apple<GameOptions>(game);
-const snake = new Snake<GameOptions>(game);
-game.addObject(apple);
-game.addObject(snake);
-
-game.input.onkeydown(params => {
-  switch(params.key) {
-    case KEY.ARROW_LEFT:
-      snake.turn(SnakeDirection.LEFT);
-      break;
-    case KEY.ARROW_UP:
-      snake.turn(SnakeDirection.UP);
-      break;
-    case KEY.ARROW_RIGHT:
-      snake.turn(SnakeDirection.RIGHT);
-      break;
-    case KEY.ARROW_DOWN:
-      snake.turn(SnakeDirection.DOWN);
-      break;
-  }
-});
-
+const game = new GameSnake(canvas);
 game.start();
